9c6310e03bfad0b22cef601ab0b7f4ae1133e05a
Agent-Red <Redr2d25@outlook.de>
2025-01-30 18:23:16 +0100

    Add files via upload

---
+++ Config.cmd
@@ -0,0 +1,14 @@
+:: This is the config file for the multitool. All variables only work using one of following letters: Y (Yes) N (No) OR 0 (No) and 1 (Yes)
+:: Idk anymore. 
+:: Or do I?
+set EG=No 
+:: Animation
+set AN=Yes
+::Banner
+set BANN=Yes
+:: Doesn't work?? 
+set ABE=Yes
+:: Doesn't work. I am proud of my 100% bug free program.
+set CC=Yes
+:: If you don't know what this setting does from reading the variable name, there is no help for you.
+set Colour=A
